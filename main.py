32da5fd7956599fe4b6bea1f0f1ccab497c300a2
RoyLeviGit <tolevi@gmail.com>
2021-11-30 14:46:26 -0500

    Added template notebooks

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('winequality-red.csv', sep=';')
+df = pd.read_csv('winequality-white.csv', sep=';')
 data = df.to_numpy()
+
 X = preprocessing.scale(data[:, :-1])
 y = data[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.5, random_state=42)
@@ -22,7 +23,7 @@ def sklearn_fit():
         print(f'{i}: Accuracy on test data = {log_reg.score(X_test, y_test)}')
 
 
-# sklearn_fit()
+sklearn_fit()
 
 X_train_1 = np.hstack((np.ones(X_train.shape[0]).reshape(-1, 1), X_train))
 y_train_2d = y_train.reshape(-1, 1)
